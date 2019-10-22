8a09a0bfbffd287fb25ef8f018f864f4817e46d7
yaoc1996 <yaoc1996@gmail.com>
2019-10-22 20:36:10 +0000

    updated proj2

--- proj2/pointcloud.py
+++ proj2/pointcloud.py
@@ -6,6 +6,8 @@ class PointCloud:
         self.axis = None
         self.transform = None
         self.points = None
+        self.homogeneous_points = None
+        self.intensities = None
     
     def read_from_file(self, fn):
         import numpy as np
@@ -26,8 +28,39 @@ class PointCloud:
                 list(map(int, f.readline().split())),
             ]
             
-            self.points = np.zeros(shape=(self.nrows, self.ncols, 3))
+            self.homogeneous_points = np.ones(shape=(self.nrows, self.ncols, 4))
+            self.intensities = np.zeros(shape=(self.nrows, self.ncols, 1))
             
             for c in range(self.ncols):
                 for r in range(self.nrows):
-                    self.points[r][c] += list(map(float, f.readline().split()))[:3]
+                    p = list(map(float, f.readline().split()))
+                    self.homogeneous_points[r][c][:3] = p[:3]
+                    self.intensities[r][c] += p[3]
+                    
+            self.points = self.homogeneous_points[:, :, :3]
+                    
+    def write_to_file(self, fn):
+        import numpy as np
+        with open(fn, 'w') as f:
+            f.write('{}\n'.format(self.ncols))
+            f.write('{}\n'.format(self.nrows))
+            f.write('{}\n'.format(' '.join(map(str, self.pos))))
+            f.write('{}\n'.format(' '.join(map(str, self.axis[0]))))
+            f.write('{}\n'.format(' '.join(map(str, self.axis[1]))))
+            f.write('{}\n'.format(' '.join(map(str, self.axis[2]))))
+            f.write('{}\n'.format(' '.join(map(str, self.transform[0]))))
+            f.write('{}\n'.format(' '.join(map(str, self.transform[1]))))
+            f.write('{}\n'.format(' '.join(map(str, self.transform[2]))))
+            f.write('{}\n'.format(' '.join(map(str, self.transform[3]))))
+            
+            for c in range(self.ncols):
+                for r in range(self.nrows):
+                    f.write('{} {}\n'.format(' '.join(map(str, self.points[r][c])), str(self.intensities[r][c][0])))
+                    
+    def apply_transform(self, transform):
+        import numpy as np
+        points = self.homogeneous_points.reshape((self.nrows*self.ncols, 4))
+        points = np.matmul(transform, points.transpose()).transpose()
+        
+        self.homogeneous_points = points.reshape((self.nrows, self.ncols, 4))
+        self.points = self.homogeneous_points[:, :, :3]
